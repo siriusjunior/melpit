bad41d475623e2bc2e51fceaccb2595771518ff8
siriusjunior <topdownreading@gmail.com>
2022-09-15 05:35:47 +0900

    [Add]プロフィール編集処理ルーティング,コントローラーメソッド

--- melpit/routes/web.php
+++ melpit/routes/web.php
@@ -27,4 +27,5 @@ Route::prefix('mypage')
     ->middleware('auth')
     ->group(function(){
         Route::get('edit-profile','ProfileController@showProfileEditForm')->name('mypage.edit-profile');
+        Route::post('edit-profile','ProfileController@editProfile')->name('mypage.edit-profile');
     });
