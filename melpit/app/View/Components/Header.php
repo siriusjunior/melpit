13d6dba20b05f851bcc622487748acedf7b834df
siriusjunior <topdownreading@gmail.com>
2022-09-16 10:49:32 +0900

    [Add]ヘッダーコンポーネントにデータ渡し

--- melpit/app/View/Components/Header.php
+++ melpit/app/View/Components/Header.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
 
 class Header extends Component
@@ -23,6 +24,8 @@ class Header extends Component
      */
     public function render()
     {
-        return view('components.header');
+        $user = Auth::user();
+        return view('components.header')
+            ->with('user',$user);
     }
 }
--- melpit/resources/views/components/header.blade.php
+++ melpit/resources/views/components/header.blade.php
@@ -1,47 +1,31 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
+            <img src="/images/logo-1.png" style="height: 39px;" alt="Melpit">
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
                 @guest
+                    {{-- 非ログイン --}}
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="btn btn-secondary ml-3" href="{{ route('register') }}" role="button">会員登録</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
+                    <li class="nav-item">
+                        <a class="btn btn-outline-info ml-2" href="{{ route('login') }}" role="button">ログイン</a>
+                    </li>
                 @else
-                    <li class="nav-item dropdown">
+                    {{-- ログイン済み --}}
+                    <li class="nav-item dropdown ml-2">
+                        {{-- ログイン情報 --}}
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            @if (!empty($user->avatar_file_name))
+                                <img src="/storage/avatars/{{$user->avatar_file_name}}" class="rounded-circle" style="object-fit: cover; width: 35px; height: 35px;">
+                            @else
+                                <img src="/images/avatar-default.svg" class="rounded-circle" style="object-fit: cover; width: 35px; height: 35px;">
+                            @endif
+                            {{ $user->name }} <span class="caret"></span>
                         </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
                     </li>
                 @endguest
             </ul>
