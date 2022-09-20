f643c4984a763481212d94bf92669c9af743af1e
siriusjunior <topdownreading@gmail.com>
2022-09-21 05:58:29 +0900

    [Modify]Eager LoadingによるN+1問題解決

--- melpit/app/Http/Controllers/SellController.php
+++ melpit/app/Http/Controllers/SellController.php
@@ -9,7 +9,15 @@ class SellController extends Controller
 {
     public function showSellForm()
     {
-        $categories = PrimaryCategory::orderBy('sort_no')->get();
+        $categories = PrimaryCategory::query()
+            ->with([
+                'secondaryCategories' => function($query){
+                    $query->orderBy('sort_no');
+                }
+            ])
+            ->orderBy('sort_no')
+            ->get();
+            
         $conditions = ItemCondition::orderBy('sort_no')->get();
         return view('sell')
             ->with('categories', $categories)
